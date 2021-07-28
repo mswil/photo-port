945dee04145acb6c39e3b316a36b388f9313af26
Molly Wilson <molly_wilson1@outlook.com>
2021-07-28 13:47:34 -0500

    Create the Photo Gallery

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
-import './App.css';
+// import './App.css';
 
 function App() {
   const [categories] = useState([
@@ -25,10 +25,8 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
-        </div>
       </main>
     </div>
   );
